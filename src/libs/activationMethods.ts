6b821be384048a868dd83f60f5717edfa019af54
pawel <ppieniacki@gmail.com>
2019-12-02 17:53:52 +0100

    added bipolar sigmoid function

--- src/libs/activationMethods.ts
+++ src/libs/activationMethods.ts
@@ -1,27 +1,23 @@
 function sigmoid(x: number): number {
-    return 1 / (1 + Math.pow(Math.E, -x));
+    return 1 / (1 + Math.exp(-x));
 }
 sigmoid.toString = () => 'sigmoid';
 sigmoid.derivative = (x: number) => x * (1 - x);
 
-// function bipolarSigmoid(x: number): number {
-//     return 1 / (1 + Math.pow(Math.E, -x));
-// }
-// bipolarSigmoid.toString = () => "bipolarSigmoid";
+function bipolarSigmoid(x: number): number {
+    return (1 - Math.exp(-x)) / (1 + Math.exp(-x));
+}
+bipolarSigmoid.toString = () => 'bipolarSigmoid';
+sigmoid.derivative = (x: number) => 2 * Math.exp(x) / ((Math.exp(x) + 1) ** 2);
 
 function bypass(x: number): number {
     return x;
 }
 bypass.toString = () => 'bypass';
-
-function round(x: number): number {
-    return Math.round(x);
-}
-round.toString = () => 'round';
+sigmoid.derivative = (x: number) => 1;
 
 export {
     sigmoid,
-    // bipolarSigmoid,
+    bipolarSigmoid,
     bypass,
-    round,
 };
---
+++ src/libs/consts.ts
@@ -0,0 +1,5 @@
+const LEARNING_FACTOR = 10;
+
+export {
+    LEARNING_FACTOR
+};
