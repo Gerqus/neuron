c7315bb2addc490aa049330b252e081ea94a87d4
ppieniacki <ppien@softserveinc.com>
2019-12-03 15:15:23 +0100

    defined activation function type

--- src/libs/activationMethods.ts
+++ src/libs/activationMethods.ts
@@ -1,20 +1,26 @@
-function sigmoid(x: number): number {
+export interface ActivationFunctionSchema {
+    (x: number): number;
+    toString: () => string;
+    derivative: (x: number) => number;
+}
+
+const sigmoid: ActivationFunctionSchema = function (x: number): number {
     return 1 / (1 + Math.exp(-x));
-}
+};
 sigmoid.toString = () => 'sigmoid';
-sigmoid.derivative = (x: number) => x * (1 - x);
+sigmoid.derivative = (x) => x * (1 - x);
 
-function bipolarSigmoid(x: number): number {
+const bipolarSigmoid: ActivationFunctionSchema = function(x: number): number {
     return (1 - Math.exp(-x)) / (1 + Math.exp(-x));
-}
+};
 bipolarSigmoid.toString = () => 'bipolarSigmoid';
-sigmoid.derivative = (x: number) => 2 * Math.exp(x) / ((Math.exp(x) + 1) ** 2);
+bipolarSigmoid.derivative = (x) => 2 * Math.exp(x) / ((Math.exp(x) + 1) ** 2);
 
-function bypass(x: number): number {
+const bypass: ActivationFunctionSchema = function(x: number): number {
     return x;
-}
+};
 bypass.toString = () => 'bypass';
-sigmoid.derivative = (x: number) => 1;
+bypass.derivative = () => 1;
 
 export {
     sigmoid,
