9527c143558384a834015d600de0e25ed82abc84
Pawel Pieniacki <ppieniacki@gmail.com>
2020-01-01 16:18:07 +0100

    reorganized activation functions code

--- src/libs/activationFunctions.ts
+++ src/libs/activationFunctions.ts
@@ -3,40 +3,45 @@ import { ActivationFunctionSchema, ActivationFunctionNames } from '../classes/Ac
 const sigmoid: ActivationFunctionSchema = function (x: number): number {
     return 1 / (1 + Math.exp(-x));
 };
+sigmoid.derivative = function (x: number) {
+    return x * (1 - x);
+}
 sigmoid.toString = () => ActivationFunctionNames.SIGMOID;
-sigmoid.derivative = (x: number) => x * (1 - x);
 
 
 const bipolarSigmoid: ActivationFunctionSchema = function(x: number): number {
     return (1 - Math.exp(-x)) / (1 + Math.exp(-x));
 };
+bipolarSigmoid.derivative = (x: number) => {
+    return 2 * Math.exp(x) / ((Math.exp(x) + 1) ** 2);
+}
 bipolarSigmoid.toString = () => ActivationFunctionNames.BIPOLAR_SIGMOID;
-bipolarSigmoid.derivative = (x: number) => 2 * Math.exp(x) / ((Math.exp(x) + 1) ** 2);
 
 
 const bypass: ActivationFunctionSchema = function(x: number): number {
     return x;
 };
+bypass.derivative = function () {
+    return 1;
+}
 bypass.toString = () => ActivationFunctionNames.BYPASS;
-bypass.derivative = () => 1;
 
 
 const ELU: ActivationFunctionSchema = function(x: number): number {
     return x >= 0 ? x : this.params.alpha * (Math.exp(x) - 1);
 };
-ELU.toString = () => ActivationFunctionNames.ELU;
 ELU.derivative = function(x: number): number {
     return x >= 0 ? 1 : this.params.alphs * Math.exp(x);
 };
 ELU.params = {
     alpha: 0.1,
 };
+ELU.toString = () => ActivationFunctionNames.ELU;
 
 
 const swish: ActivationFunctionSchema = function(x: number): number {
     return x * sigmoid(x);
 };
-swish.toString = () => ActivationFunctionNames.SWISH;
 swish.derivative = function(x: number): number {
     // return (Math.exp(-x) * (x + 1) + 1) / Math.pow(1 + Math.exp(-x), 2);
     return sigmoid(x * this.params.beta) + x * this.params.beta * sigmoid(x * this.params.beta) * (1 - sigmoid(x * this.params.beta));
@@ -44,6 +49,7 @@ swish.derivative = function(x: number): number {
 swish.params = {
     beta: 1,
 };
+swish.toString = () => ActivationFunctionNames.SWISH;
 
 
 export const ActivationFunctions = {
