18b65870cd71dd4c8ed8937d53eed6cdbc5fd51c
Pawel Pieniacki <ppieniacki@gmail.com>
2022-11-22 20:53:36 +0100

    added lab to exported libs

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-export { Network, NetworkSchema } from './classes/Network.class';
-export { NeuronSchema } from './classes/Neuron.class';
-export { LayerSchema } from './classes/Layer.class';
-export { ConnectionSchema } from './classes/Connection.class';
-export { ActivationFunctionsNames } from './libs/activationFunctions';
-export { ErrorFunctionsNames } from './libs/errorFunctions';
-export { dataset } from './interfaces/dataset.interface';
-export { TestData as testData } from './interfaces/test-data.interface';
-
+export * from './classes/Network.class';
+export * from './classes/Neuron.class';
+export * from './classes/Layer.class';
+export * from './classes/Connection.class';
+export * from './libs/activationFunctions';
+export * from './libs/errorFunctions';
+export * from './libs/lab';
+export * from './interfaces/dataset.interface';
+export * from './interfaces/test-data.interface';
 export * from './xor';
